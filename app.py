d9e1e673433afd430c7330c136e06c8ca595ef50
DivyamGupta11 <divyam@thebrownacademy.com>
2025-01-08 11:01:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-LOGGLY_URL = "http://logs-01.loggly.com/inputs/bbdf6498-55d9-4642-8cce-67eaca20be35/tag/http/"
+LOGGLY_URL_DIVYAM = "http://logs-01.loggly.com/inputs/8e7bde5a-6e51-4cb5-b573-31b73a532961/tag/http/"
 HEADERS = {"Content-Type": "application/json"}
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(asctime)s - %(message)s")
@@ -20,16 +20,16 @@ def decode_email(encoded_email):
     return base64.urlsafe_b64decode(encoded_email.encode('utf-8')).decode('utf-8')
 
 # Send log data to Loggly
-def send_log_to_loggly(email, timestamp):
+def send_log_to_loggly(url,email, timestamp):
     log_data = {
         "email": email,
         "timestamp": timestamp
     }
-    response = requests.post(LOGGLY_URL, json=log_data, headers=HEADERS)
+    response = requests.post(url, json=log_data, headers=HEADERS)
     if response.status_code != 200:
         logging.error(f"Failed to send log to Loggly: {response.text}")
 
-REDIRECT_URL = "https://www.talenthr.io"
+REDIRECT_URL = "https://docs.google.com/spreadsheets/u/1/"
 
 @app.route("/verify", methods=["GET"])
 def verify_click():
@@ -44,7 +44,7 @@ def verify_click():
     timestamp = datetime.now().strftime("%I:%M:%S %p, %B %d, %Y")
     
     # Log the click to Loggly
-    send_log_to_loggly(email, timestamp)
+    send_log_to_loggly(LOGGLY_URL_DIVYAM,email, timestamp)
 
     # Redirect to the target website
     return redirect(REDIRECT_URL, code=302)
